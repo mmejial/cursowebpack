d44030be0de662b671cc9d27a4789c36578c5276
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2021-05-24 15:59:59 -0500

    se crearon los alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin =require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin= require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
 
     output:{
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
 
     },
     resolve:{
-        extensions: ['.js']//dependiento de lo que se utulice en el proyecto se ponen las extenciones
+        extensions: ['.js'],//dependiento de lo que se utulice en el proyecto se ponen las extenciones
+        alias:{
+            '@utils': path.resolve(__dirname,'src/utils/'),
+            '@templates': path.resolve(__dirname,'src/templates/'),
+            '@styles': path.resolve(__dirname,'src/styles/'),
+            '@images': path.resolve(__dirname,'src/assets/images/'),
+        }
     },
     module:{
         rules:[{
@@ -22,9 +32,28 @@ module.exports = {
             }
         },
         {
-         test:/\.css$/,
-         use:[MiniCssExtractPlugin.loader, 'css-loader'],   
-        }
+         test:/\.s?css$/,
+         use:[MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],   
+        },
+        {
+        test: /\.png/,
+        type: 'asset/resource'
+        },
+        {
+            test: /\.(woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff",
+                    name: "[name].[contenthash].[ext]",
+                    outputPath: "./assets/fonts/",
+                    publicPath: "../assets/fonts/",
+                    esModule: false,
+
+                },
+            }
+        },
     ]
 
     },
@@ -34,7 +63,25 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns:[{
+                from: path.resolve(__dirname, "src","assets/images"),
+                to: "assets/images"
+            }
+
+            ]
+        })
     ],
+    optimization:{
+        minimize:true,
+        minimizer:[
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
+    
 
 }
